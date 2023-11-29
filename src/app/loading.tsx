4ca8a9df4776814118ef13550377df4c3c65c9b1
Jonothan Sue <jonothansue@gmail.com>
2023-11-30 09:44:52 +1100

    add basic homepage

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,12 +2,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex items-center space-x-4">
+    <div className="flex items-center justify-center space-x-4 p-4">
       <Skeleton className="h-12 w-12 rounded-full" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-[250px]" />
         <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24 gap-8">
+      <h1 className="text-8xl font-thin">
+        Welcome to <span className="font-bold">GoFetch</span>
+      </h1>
+      <p>Learn a language through the content you love &#x2764;</p>
     </main>
-  )
+  );
 }

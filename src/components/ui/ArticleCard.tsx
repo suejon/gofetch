4bc7b053114265e099db6725f676790c8181e89e
Jonothan Sue <jonothansue@gmail.com>
2024-01-06 21:15:22 +0800

    images: use placehodlers

--- src/components/ui/ArticleCard.tsx
+++ src/components/ui/ArticleCard.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./button";
 import img from "../../../public/bedbug.jpg";
+import placeholder from "../../../public/placeholder.webp";
 
 interface Props {
   articleId: number;
   title: string;
+  image: string;
   thumbnail: string;
   lf: string;
   lf_level: string;
@@ -14,6 +16,7 @@ interface Props {
 export default function ArticleCard({
   articleId,
   title,
+  image,
   thumbnail,
   lf,
   lf_level,
@@ -21,8 +24,14 @@ export default function ArticleCard({
   return (
     <div className="border-black border-2 rounded-md flex-col gap-2 p-4 max-w-lg">
       <div className="flex justify-center h-72">
+        {/* <Image */}
+        {/*   src={img} */}
+        {/*   style={{ objectFit: "cover" }} */}
+        {/*   alt="thumbnail" */}
+        {/*   blurDataURL="/placeholder.webp" */}
+        {/* /> */}
         <Image
-          src={img}
+          src={placeholder}
           style={{ objectFit: "cover" }}
           alt="thumbnail"
           blurDataURL="/placeholder.webp"

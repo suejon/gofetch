4ad9272c915e74ce0c42d941fa79a99b4fabefb6
Jonothan Sue <jonothansue@gmail.com>
2024-01-21 10:33:08 +1300

    add brand image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
+import dog from "../../public/gofetch-dog.svg";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-8">
+      <Image src={dog} alt="gofetch" className="h-52" />
       <h1 className="text-8xl font-thin">
         Welcome to <span className="font-bold">GoFetch</span>
       </h1>
